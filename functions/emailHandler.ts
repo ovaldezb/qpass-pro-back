ee44fca663db0500965287352fbf6898102c2c83
Sari Fernandez <sarifern@cisco.com>
2023-03-22 14:18:39 -0600

    email template with QR (not calculated)

--- functions/emailHandler.ts
+++ functions/emailHandler.ts
@@ -1,72 +1,141 @@
 var nodemailer1 = require('nodemailer')
+var handlebars = require('handlebars')
+var email_html = `
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Apartment Invitation</title>
+    <style>
+      /* Add your custom CSS styles here */
+      body {
+        font-family: Arial, sans-serif;
+        margin: 0;
+        padding: 0;
+      }
+      .qr-code {
+        max-width: 200px;
+        margin: 10px;
+      }
+      
+      .header {
+        background-color: #2D3E50;
+        color: #ffffff;
+        padding: 20px;
+        text-align: center;
+      }
+      
+      .content {
+        padding: 20px;
+        text-align: left;
+      }
+      
+      .footer {
+        background-color: #2D3E50;
+        color: #ffffff;
+        padding: 20px;
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="header">
+      <h1> Atenta Invitación </h1>
+    </div>
+    <div class="content">
+      <p>Estimado {{ nombreInvitado }},</p>
+      <p>Estas cordialmente invitado a un evento, el cual se llevará a cabo en {{ lugar }}, el día {{ fechaEvento }} a las {{ horaEvento}}.</p>
+      <p>Por favor considera las reglas de nuestro condominio:</p>
+      <ul>
+        <li>No fumar en zonas cerradas.</li>
+        <li>No se permitirá música despues de las 10 pm.</li>
+        <li>No se permiten mascotas.</li>
+      </ul>
+      <p>Para poder ingresar al complejo, favor de presentar el siguiente código QR en seguridad:</p>
+      <img class="qr-code" src="{{ qr_url }}" alt="QR Code" />
+      <p>Esperamos recibirlo pronto!</p>
+      <p>Saludos,</p>
+      <p>{{ anfitrion }}</p>
+    </div>
+    <div class="footer">
+      <p>Copyright © 2023 {{ nombreCondominio }}</p>
+    </div>
 
+  </body>
+</html>
+`;
 var transporter = nodemailer1.createTransport({
-  host:process.env.EMAIL_HOST,
-  port:process.env.EMAIL_PORT,
-  secure:true,
-  auth:{
+  host: process.env.EMAIL_HOST,
+  port: process.env.EMAIL_PORT,
+  secure: true,
+  auth: {
     type: 'login',
-    user:process.env.EMAIL_USER,
-    pass:process.env.EMAIL_PASS
+    user: process.env.EMAIL_USER,
+    pass: process.env.EMAIL_PASS
   },
   ignoreTLS: true,
   tls: {
-   // do not fail on invalid certs
-   rejectUnauthorized: false
+    // do not fail on invalid certs
+    rejectUnauthorized: false
   }
 });
 
-exports.handler = (event: any, context: any, callback: any) =>{
+exports.handler = (event: any, context: any, callback: any) => {
   console.log('Enviando correo');
-  console.log('Recieved Event:'+JSON.stringify(event, null, 4) );
+  console.log('Recieved Event:' + JSON.stringify(event, null, 4));
   var message = event.Records[0].Sns.Message;
   console.log(message);
-  //let _body = JSON.parse(event.body);
-  //console.log(_body);
-    let body_txt='<h2>Contact Form Details</h2>';
-    /*for (var key in _body) {
-        var res = key.replace("_", " ");
-        body_txt +='<p><strong>' + titleCase(res) +' : </strong>'+_body[key] + '</p>';
-    }*/
-    //console.log(body_txt);
+  var message = JSON.parse(message);
+  var template = handlebars.compile(email_html);
+  var replacements = {
+    anfitrion: message.anfitrion,
+    qr_url: 'https://chart.googleapis.com/chart?chs=200x200&cht=qr&chl=youtu.be/dQw4w9WgXcQ&choe=UTF-8',
+    lugar: message.lugar,
+    fechaEvento:message.fechaEvento,
+    horaEvento: message.horaEvento,
+    nombreInvitado: message.nombreInvitado,
+    nombreCondominio: "Quinta Palmeras"
+  };
+  var htmlToSend = template(replacements);
 
-    let mailOptions = {
-        from: 'soporte@gecoplus.mx',
-        to: 'omar.valdez.becerril@gmail.com',
-        subject: 'EmailHandler',
-        html: body_txt
-    };
+  let mailOptions = {
+    from: 'soporte@gecoplus.mx',
+    to: 'marianasari@gmail.com,hheisego@cisco.com,omar.valdez.becerril@gmail.com',
+    subject: "Invitacion para "+message.asunto,
+    html: htmlToSend
+  };
 
-    transporter.verify((err:any, success:any)=>{
-      if(err){
-        console.log(err);
-      }else{
-        console.log('Server is ready');
-      }
-    });
-
-    var response = transporter.sendMail(mailOptions);
-    console.log(response);
-    transporter.close();
-
-    return {
-      statusCode:200,
-      body:'Se envio',
-      headers: {
-        'Content-Type': 'application/json',
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'POST'
-      }
+  transporter.verify((err: any, success: any) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log('Server is ready');
     }
+  });
+
+  var response = transporter.sendMail(mailOptions);
+  console.log(response);
+  transporter.close();
+
+  return {
+    statusCode: 200,
+    body: 'Se envio',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'POST'
+    }
+  }
 }
 
 function titleCase(str: string) {
-    var splitStr = str.toLowerCase().split(' ');
-    for (var i = 0; i < splitStr.length; i++) {
-        // You do not need to check if i is larger than splitStr length, as your for does that for you
-        // Assign it back to the array
-        splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
-    }
-    // return the joined string
-    return splitStr.join(' ');
+  var splitStr = str.toLowerCase().split(' ');
+  for (var i = 0; i < splitStr.length; i++) {
+    // You do not need to check if i is larger than splitStr length, as your for does that for you
+    // Assign it back to the array
+    splitStr[i] = splitStr[i].charAt(0).toUpperCase() + splitStr[i].substring(1);
+  }
+  // return the joined string
+  return splitStr.join(' ');
 }
