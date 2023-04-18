da4954ab376facdca13c5a71dcd80c8c107a41eb
Sari Fernandez <sarifern@cisco.com>
2023-04-18 17:23:27 -0600

    initial cognito configuration with User/Password

---
+++ lib/protected-apis.ts
@@ -0,0 +1,132 @@
+import { Duration } from 'aws-cdk-lib';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
+import * as iam from 'aws-cdk-lib/aws-iam';
+import { LambdaRestApi, EndpointType, Cors, AuthorizationType,
+    IdentitySource, LambdaIntegration, RequestAuthorizer,
+} from 'aws-cdk-lib/aws-apigateway';
+import { Runtime } from 'aws-cdk-lib/aws-lambda';
+import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { Construct } from 'constructs';
+import { join } from 'path';
+type ProtectedApiProps = {
+	userPoolId: string;
+	userPoolClientId: string;
+};
+
+export class ProtectedApis extends Construct {
+	constructor(scope: Construct, id: string, props: ProtectedApiProps) {
+		super(scope, id);
+
+        const snsTopic = new sns.Topic(this,'sns-topic',{
+            topicName:'EmailTopic',
+            displayName:'Email Topic'
+          });
+      
+          const nodeJsFunctionProps: NodejsFunctionProps = {
+            bundling: {
+              externalModules: [
+                'aws-sdk'
+              ]
+            },
+            environment: {
+              MONGODB_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
+              TOPIC_ARN:snsTopic.topicArn,
+              USER_POOL_ID: props.userPoolId,
+			  CLIENT_ID: props.userPoolClientId,
+            },
+            runtime: Runtime.NODEJS_16_X
+          }
+          const nodeJsEmailProps: NodejsFunctionProps = {
+            bundling: {
+              externalModules: [
+                'aws-sdk'
+              ]
+            },
+            environment: {
+              CRYPTO_KEY:`${process.env.CRYPTO_KEY}`,
+              CRYPTO_IV:`${process.env.CRYPTO_IV}`,
+              EMAIL_USER:`${process.env.EMAIL_USER}`,
+              EMAIL_PASS:`${process.env.EMAIL_PASS}`,
+              EMAIL_HOST:`${process.env.EMAIL_HOST}`,
+              EMAIL_PORT:`${process.env.EMAIL_PORT}`
+            },
+            runtime: Runtime.NODEJS_16_X
+          }
+          const invitacionFunction = new NodejsFunction(this, 'InvitadosFunction', {
+            functionName: 'invitadosFunction',
+            entry: join(__dirname, '/../functions/invitacionHandler.ts'),
+            ...nodeJsFunctionProps
+          });
+      
+          const snsTopicPolicy = new iam.PolicyStatement({
+            actions:['sns:publish'],
+            resources:['*']
+          });
+      
+          invitacionFunction.addToRolePolicy(snsTopicPolicy);
+      
+          const emailSendFunction = new NodejsFunction(this,'EnvioEmail',{
+            functionName:'EnvioEmail',
+            entry: join(__dirname,'/../functions/emailHandler.ts'),
+            ...nodeJsEmailProps
+          });
+          
+          snsTopic.addSubscription(new subs.LambdaSubscription(emailSendFunction));
+      
+          const apiGw = new LambdaRestApi(this, 'InvitacionApiGw', {
+            restApiName: 'Invitacion Service',
+            handler: invitacionFunction,
+            proxy: false,
+            deployOptions: {
+              stageName: 'dev'
+            }
+          });
+      
+          const apiGwEmail = new LambdaRestApi(this,'EmailApiG',{
+            restApiName: 'EnvioEmail',
+            handler: emailSendFunction,
+            proxy: false,
+            deployOptions:{
+              stageName:'dev'
+            }
+          });
+          const authorizerFn = new NodejsFunction(this, 'AuthorizerFn', {
+			...nodeJsFunctionProps,
+			entry: './functions/auth/authorizer.ts',
+		});
+
+		const requestAuthorizer = new RequestAuthorizer(this, 'RequestAuthorizer', {
+			identitySources: [IdentitySource.header('cookie')],
+			handler: authorizerFn,
+			resultsCacheTtl: Duration.minutes(0),
+		});
+
+
+          const email = apiGwEmail.root.addResource('email');
+          email.addMethod('POST');
+          
+          const invitacion = apiGw.root.addResource('invitacion');
+          invitacion.addMethod('GET', new LambdaIntegration(invitacionFunction),{
+            authorizer: requestAuthorizer,
+            authorizationType: AuthorizationType.CUSTOM
+          });
+          invitacion.addMethod('POST', new LambdaIntegration(invitacionFunction),{
+            authorizer: requestAuthorizer,
+            authorizationType: AuthorizationType.CUSTOM
+          });
+          const singleInvitacion = invitacion.addResource('{idInvitacion}');
+          singleInvitacion.addMethod('GET', new LambdaIntegration(invitacionFunction),{
+            authorizer: requestAuthorizer,
+            authorizationType: AuthorizationType.CUSTOM
+          });
+          singleInvitacion.addMethod('PUT', new LambdaIntegration(invitacionFunction),{
+            authorizer: requestAuthorizer,
+            authorizationType: AuthorizationType.CUSTOM
+          });
+          singleInvitacion.addMethod('DELETE', new LambdaIntegration(invitacionFunction),{
+            authorizer: requestAuthorizer,
+            authorizationType: AuthorizationType.CUSTOM
+          });
+    }
+}
--- lib/qpass-pro-back-stack.ts
+++ lib/qpass-pro-back-stack.ts
@@ -1,97 +1,26 @@
 import * as cdk from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as iam from 'aws-cdk-lib/aws-iam';
-import { LambdaRestApi } from 'aws-cdk-lib/aws-apigateway';
-import { Runtime } from 'aws-cdk-lib/aws-lambda';
-import { NodejsFunction, NodejsFunctionProps } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { AuthApi } from './auth-api';
+import { ProtectedApis } from './protected-apis';
+import { CognitoUserPool } from './user-pool';
 import { Construct } from 'constructs';
-import { join } from 'path';
+
 
 export class QpassProBackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
+    const userPool = new CognitoUserPool(this, 'UserPool');
 
-    const snsTopic = new sns.Topic(this,'sns-topic',{
-      topicName:'EmailTopic',
-      displayName:'Email Topic'
-    });
+		const { userPoolId, userPoolClientId } = userPool;
 
-    const nodeJsFunctionProps: NodejsFunctionProps = {
-      bundling: {
-        externalModules: [
-          'aws-sdk'
-        ]
-      },
-      environment: {
-        MONGODB_URI: `mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PW}@${process.env.MONGO_HOST}/${process.env.MONGO_DB}?retryWrites=true&w=majority`,
-        TOPIC_ARN:snsTopic.topicArn
-      },
-      runtime: Runtime.NODEJS_16_X
-    }
-    const nodeJsEmailProps: NodejsFunctionProps = {
-      bundling: {
-        externalModules: [
-          'aws-sdk'
-        ]
-      },
-      environment: {
-        CRYPTO_KEY:`${process.env.CRYPTO_KEY}`,
-        CRYPTO_IV:`${process.env.CRYPTO_IV}`,
-        EMAIL_USER:`${process.env.EMAIL_USER}`,
-        EMAIL_PASS:`${process.env.EMAIL_PASS}`,
-        EMAIL_HOST:`${process.env.EMAIL_HOST}`,
-        EMAIL_PORT:`${process.env.EMAIL_PORT}`
-      },
-      runtime: Runtime.NODEJS_16_X
-    }
-    const invitacionFunction = new NodejsFunction(this, 'InvitadosFunction', {
-      functionName: 'invitadosFunction',
-      entry: join(__dirname, '/../functions/invitacionHandler.ts'),
-      ...nodeJsFunctionProps
-    });
+		new AuthApi(this, 'AuthServiceApi', {
+			userPoolId,
+			userPoolClientId,
+		});
 
-    const snsTopicPolicy = new iam.PolicyStatement({
-      actions:['sns:publish'],
-      resources:['*']
-    });
-
-    invitacionFunction.addToRolePolicy(snsTopicPolicy);
-
-    const emailSendFunction = new NodejsFunction(this,'EnvioEmail',{
-      functionName:'EnvioEmail',
-      entry: join(__dirname,'/../functions/emailHandler.ts'),
-      ...nodeJsEmailProps
-    });
+		new ProtectedApis(this, 'ProtectedApis', {
+			userPoolId,
+			userPoolClientId,
+		});
     
-    snsTopic.addSubscription(new subs.LambdaSubscription(emailSendFunction));
-
-    const apiGw = new LambdaRestApi(this, 'InvitacionApiGw', {
-      restApiName: 'Invitacion Service',
-      handler: invitacionFunction,
-      proxy: false,
-      deployOptions: {
-        stageName: 'dev'
-      }
-    });
-
-    const apiGwEmail = new LambdaRestApi(this,'EmailApiG',{
-      restApiName: 'EnvioEmail',
-      handler: emailSendFunction,
-      proxy: false,
-      deployOptions:{
-        stageName:'dev'
-      }
-    });
-    const email = apiGwEmail.root.addResource('email');
-    email.addMethod('POST');
-    
-    const invitacion = apiGw.root.addResource('invitacion');
-    invitacion.addMethod('GET');
-    invitacion.addMethod('POST');
-    const singleInvitacion = invitacion.addResource('{idInvitacion}');
-    singleInvitacion.addMethod('GET');
-    singleInvitacion.addMethod('PUT');
-    singleInvitacion.addMethod('DELETE');
   }
 }
