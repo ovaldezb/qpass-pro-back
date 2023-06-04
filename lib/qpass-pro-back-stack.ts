3daef6cf30224a2ed6315944a4433e8be810fb1d
Sari Fernandez <sarifern@cisco.com>
2023-06-04 16:28:32 -0600

    Amplify Cognito as Authorizer

--- lib/qpass-pro-back-stack.ts
+++ lib/qpass-pro-back-stack.ts
@@ -1,26 +1,17 @@
-import * as cdk from 'aws-cdk-lib';
-import { AuthApi } from './auth-api';
-import { ProtectedApis } from './protected-apis';
-import { CognitoUserPool } from './user-pool';
-import { Construct } from 'constructs';
-
+import * as cdk from "aws-cdk-lib";
+import { ProtectedApis } from "./protected-apis";
+import { Construct } from "constructs";
 
 export class QpassProBackStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-    const userPool = new CognitoUserPool(this, 'UserPool');
 
-		const { userPoolId, userPoolClientId } = userPool;
+    const userPoolId = process.env.COGNITO_USER_POOL_ID as string;
+    const userPoolClientId = process.env.COGNITO_USER_POOL_CLIENT_ID as string;
 
-		new AuthApi(this, 'AuthServiceApi', {
-			userPoolId,
-			userPoolClientId,
-		});
-
-		new ProtectedApis(this, 'ProtectedApis', {
-			userPoolId,
-			userPoolClientId,
-		});
-    
+    new ProtectedApis(this, "ProtectedApis", {
+      userPoolId,
+      userPoolClientId,
+    });
   }
 }
